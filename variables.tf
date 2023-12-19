b286af6d7eefd9ca9ab3a57861053232a46e646a
mahesh2121 <maheshgaikwad128@gmail.com>
2023-12-19 13:11:33 +0530

    fixed description

--- variables.tf
+++ variables.tf
@@ -1,20 +1,23 @@
 variable "environment" {
   type        = string
   default     = "deni"
-  description = "description"
+  description = "Environment Prod  Dev Stag"
 }
 
 variable "region" {
   type = string
+  description = "Environment region"
 
 }
 
 variable "availability_zones" {
   type = list(any)
+  description = " List available AZ in region"
 }
 
 variable "bastion_instance_type" {
   type = string
+  description = "bastion host system family type"
 }
 
 variable "cidr_block" {
