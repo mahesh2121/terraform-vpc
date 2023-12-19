88eb7ac7c4dd0cd86eab900cc4a8b68bcf2479dd
mahesh2121 <maheshgaikwad128@gmail.com>
2023-12-19 13:09:40 +0530

    fixed string inter

--- subnet.tf
+++ subnet.tf
@@ -2,7 +2,12 @@ resource "aws_subnet" "public" {
   count                   = length(var.availability_zones)
   vpc_id                  = aws_vpc.main.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, count.index)
-  availability_zone      = element(var.availability_zones, count.index)
+  availability_zone       = element(var.availability_zones, count.index)
   map_public_ip_on_launch = true
+
+  tags = {
+
+    "Name" = "Public-subnet - ${element(var.availability_zone,  count.index)}"
+  }
 }
 
